e5d07433674b54e8a9314237042c8f43ee49de33
Fernando Nava <fnava621@gmail.com>
2012-11-14 19:04:22 -0800

    fix heroku deploy bug

--- adn_news.py
+++ adn_news.py
@@ -591,4 +591,4 @@ def add_comments(post_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host'0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
